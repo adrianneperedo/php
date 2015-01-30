d6df51b946406759e67cfa523783d4249877662a
root <root@UNIT120.(none)>
2015-01-30 17:07:30 +0800

    Create Chap05 folder and arrays.php

---
+++ Chap05/arrays.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Arrays</title>
+</head>
+<body>
+	<h1>Arrays</h1>
+	
+	<h2>range()</h2>
+	<?php 
+		$num = range(4, 9);
+		print_r($num);	
+	 ?>
+
+</body>
+</html>
