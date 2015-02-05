1af42565a3ef0502f29d04760f54488e40bec20e
Adrianne Peredo <a.peredo@klab.com>
2015-02-05 16:13:29 +0800

    Create Forms folder and forms.php

---
+++ Forms/forms.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Forms</title>
+</head>
+<body>
+	<form action="" method="get">
+		<input type="text" value="username" name="username1" >
+		<input type="submit" value="Login" name="Submit1">
+	</form>
+
+</body>
+</html>
