5f8700e68d1c371695ff50fd85bcfe84f3f05cfc
Adrianne Peredo <a.peredo@klab.com>
2015-02-06 16:32:56 +0800

    Add new feature: realtime countdown

--- datetime/countdown.php
+++ datetime/countdown.php
@@ -2,12 +2,15 @@
 <body>
 
 <?php
+
+header("refresh: 1");
+
 $current_time = date('h:i:s'); 
 
 $time1 = new DateTime($current_time);
 $time2 = new DateTime('05:30:00');
 $time_left = $time2->diff($time1);
-echo $time_left->format('%H:%i:%S') . " time left before weekend! Yay!";
+echo $time_left->format('%H:%I:%S') . " time left before weekend! Yay!";
 ?>
 
 </body>
