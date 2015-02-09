1e13415661b6b6ba89ce7989117cce94b57ea7f7
Adrianne Peredo <a.peredo@klab.com>
2015-02-09 14:48:01 +0800

    Modify content

--- Chap01/delete.php
+++ Chap01/delete.php
@@ -1,11 +1,10 @@
 <?php 
 	include('conn.php');
-	if (!empty($_GET)) {
 		$id = $_GET['id'];
 		
 		$sql = "DELETE FROM books WHERE id =$id";
 		$db->query($sql);
 
 		header('Location: booklist.php');
-	}
+	
  ?>
